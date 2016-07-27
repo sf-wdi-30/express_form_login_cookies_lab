85cabc937d4caeaac9af3db91525af68d1034711
justincastilla <pxlperfection@gmail.com>
2016-07-27 13:17:52 -0700

    removed/added semicolons

--- seed.js
+++ seed.js
@@ -3,20 +3,20 @@ var db = require("./models");
 var new_user = {
   username: "anon",
   password: "123"
-}
+};
 
 // Clear Database
 db.User.remove({}, function(err){
 
-  if (err) { return console.log(err); };
+  if (err) { return console.log(err); }
   console.log("---> Database Wiped");
 
   // Seed Database
   db.User.create(new_user, function(err, user){
-    if (err) { return console.log(err); };
-    console.log("---> One user added")
+    if (err) { return console.log(err); }
+    console.log("---> One user added");
     console.log(user);
     process.exit();
-  })
+  });
 
-})
+});
