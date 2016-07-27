3e89d6926273a35a6c527e1c09ac85ad5fa60360
nathanallen <thenathanator@gmail.com>
2016-07-27 11:35:23 -0700

    fixes #3 rename username to email

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose"),
 // var bcrypt = require('bcrypt');
 
 var userSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     index: {unique: true} // uniqueness constraint
