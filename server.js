e69be2e5a9e5b03adf93c9bf60f8e846c96e8542
melicarls <melissacarlson15@gmail.com>
2016-07-28 09:28:28 -0700

    solutions for 1-4

--- server.js
+++ server.js
@@ -53,18 +53,29 @@ app.post(["/login", "/api/sessions"], function createSession(req, res){
  var email = req.body.email;
  var password = req.body.password;
  // TODO#1: find the matching user in the database
- // TODO#1: set a cookie named "guid" in the HTTP Response Header
- //                             with the user's _id as the value
- // TODO#1: redirect to the profile page
+ db.User.findOne({email: email}, function(err, foundUser) {
+   console.log("Found this user: ", foundUser);
+   if (err) {return console.log(err);}
+   if (password !== foundUser.password) {
+     console.log("That password was not correct");
+     // Status code for "Unauthorized"
+     res.sendStatus(401);
+   } else {
+     console.log("Passwords match");
+     // TODO#1: set a cookie named "guid" in the HTTP Response Header
+     //                             with the user's _id as the value
+     // TODO#1: redirect to the profile page
+     res.cookie('guid',foundUser._id).redirect("/api/profile");
+   }
+ });
+ // res.redirect("/login");
  // TODO#5: securely authenticate users
- res.redirect("/login");
-
 });
 
 app.get(["/logout", "/api/sessions"], function destroySession(req, res){
  console.log("Looks like you're trying to logout!");
  // TODO#2 clear the "guid" cookie from the HTTP Response Header
- res.redirect("/");
+ res.clearCookie('guid').redirect("/login");
 });
 
 app.post(["/signup", "/api/users"], function createUser(req, res){
@@ -73,26 +84,41 @@ app.post(["/signup", "/api/users"], function createUser(req, res){
  var email = req.body.email;
  var password = req.body.password;
  // TODO#3 create a new user
+   var newUser = new db.User({
+    email: email,
+    password: password
+  });
  // TODO#3 set a cookie named "guid" in the HTTP Response Header
  //                            with the user's _id as the value
  // TODO#3 redirect to the profile page
+   newUser.save(function(err, savedUser) {
+    res.cookie('guid', savedUser._id).redirect('/api/profile');
+  });
  // TODO#5 create new users securely (don't just store plain-text passwords!)
  res.redirect("/signup");
 
 });
 
 app.get("/api/profile", function showUser(req, res){
- console.log("Looks like you're visiting the profile")
- // TODO#4 grab the value of the "guid" cookie from the HTTP Request Header
- // TODO#4 find the matching user in the database
- // TODO#4 add the user to the response JSON below
- var user = null; // change me
- res.send({
-   request_headers: req.headers,
-   user: user || "NOT FOUND"
- });
+  console.log("Looks like you're visiting the profile");
+  // If the user does not have a guid cookie, redirect to the root route
+  if (!req.cookies.guid) {
+    res.redirect("/");
+  } else {
+    // TODO#4 grab the value of the "guid" cookie from the HTTP Request Header
+    // TODO#4 find the matching user in the database
+    var userId = req.cookies.guid;
+    // Find one user by ID
+    db.User.findOne({_id: userId}, function(err, foundUser) {
+    // TODO#4 add the user to the response JSON below
+      var user = foundUser;
+      res.send({
+        request_headers: req.headers,
+        user: user || "NOT FOUND"
+      });
+    });
+  }
 });
-
 
 /**********
 * SERVER *
